c54ad4a9d81e8b738e59d68b8eb9d13cb3150a80
Sannukas <sandra.vahi@khk.ee>
2013-09-24 11:41:19 +0300

    Tags begin

--- templates/master_template.php
+++ templates/master_template.php
@@ -41,13 +41,13 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="http://localhost/blogi/"><?=PROJECT_NAME?></a>
+			<a class="navbar-brand" href="<?=BASE_URL?>"><?=PROJECT_NAME?></a>
 		</div>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="http://localhost/blogi/">Home</a></li>
-				<li><a href="#about">About</a></li>
-				<li><a href="#contact">Contact</a></li>
+				<li class="active"><a href="<?=BASE_URL?>">Posts</a></li>
+				<li><a href="<?=BASE_URL?>tags">Tags</a></li>
+				<li><a href="<?=BASE_URL?>users">Users</a></li>
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
 					<ul class="dropdown-menu">
---
+++ views/tags/tags_index.php
@@ -0,0 +1,8 @@
+<div class="list-group">
+	<a href="#" class="list-group-item active">
+		Cras justo odio
+	</a>
+	<a href="#" class="list-group-item"><span class="badge">14</span>
+		<?=$post['post_subject']?>
+	</a>
+</div>
