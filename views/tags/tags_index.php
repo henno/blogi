5cdfcdc6075052e6339b3c623d652d1c943b84e7
henno <henno.taht@diara.ee>
2013-10-01 12:59:24 +0300

    Reformat tags_index

--- views/tags/tags_index.php
+++ views/tags/tags_index.php
@@ -3,7 +3,8 @@
 		Tags
 	</a>
 	<?foreach($tags as $tag):?>
-	<a href="<?=BASE_URL?>tags/view/<?=$tag['tag_name']?>" class="list-group-item"><span class="badge"><?=$tag['count']?></span>
+	<a href="<?=BASE_URL?>tags/view/<?=$tag['tag_name']?>" class="list-group-item">
+		<span class="badge"><?=$tag['count']?></span>
 		<?=$tag['tag_name']?>
 	</a>
 	<?endforeach?>
